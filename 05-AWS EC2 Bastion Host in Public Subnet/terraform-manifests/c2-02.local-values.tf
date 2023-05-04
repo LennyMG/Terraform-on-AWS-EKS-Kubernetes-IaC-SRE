69b4ba2082bafa6ff59f122759fc4c1708ca98a4
LennyMG <lennymg.87@gmail.com>
2023-05-04 15:17:03 -0500

    day-06.2

--- 05-AWS EC2 Bastion Host in Public Subnet/terraform-manifests/c2-02.local-values.tf
+++ 05-AWS EC2 Bastion Host in Public Subnet/terraform-manifests/c2-02.local-values.tf
@@ -1,11 +1,13 @@
 # Define Local Values in Terraform
 locals {
-  owners = var.business_divsion #HR
-  environment = var.environment #dev
-  name = "${var.business_divsion}-${var.environment}" #HR-dev
+  owners      = var.business_divsion                         #HR
+  environment = var.environment                              #dev
+  name        = "${var.business_divsion}-${var.environment}" #HR-dev
   common_tags = {
-    owners = local.owners#HR 
-#     environment = local.environment dev    
-  }
+    owners = local.owners #HR 
+    environment = local.environment
+  }      
+    # Add additional local value
+    eks_cluster_name = "${local.name}-${var.cluster_name}"
 }
 
--- 05-AWS EC2 Bastion Host in Public Subnet/terraform-manifests/c3.02-vpc-modules.tf
+++ 05-AWS EC2 Bastion Host in Public Subnet/terraform-manifests/c3.02-vpc-modules.tf
@@ -1,7 +1,7 @@
 # Change-1: Add Datasource named aws_availability_zones
 # AWS Availability Zones Datasource  
 data "aws_availability_zones" "available" {
-} 
+}
 
 #Cireate VPC Terraform Module
 module "vpc" {
@@ -9,23 +9,23 @@ module "vpc" {
   version = "2.78.0"
   #version = "~> 2.0"
 
-# VPC Basic Details
+  # VPC Basic Details
   name = "${local.name}-${var.vpc_name}"
   cidr = var.vpc_cidr_block
   # azs             = var.vpc_availability_zones
-   azs             = data.aws_availability_zones.available.names
+  azs             = data.aws_availability_zones.available.names
   public_subnets  = var.vpc_public_subnets
-  private_subnets = var.vpc_private_subnets  
+  private_subnets = var.vpc_private_subnets
 
   # Database Subnets
-  database_subnets = var.vpc_database_subnets
-  create_database_subnet_group = var.vpc_create_database_subnet_group
+  database_subnets                   = var.vpc_database_subnets
+  create_database_subnet_group       = var.vpc_create_database_subnet_group
   create_database_subnet_route_table = var.vpc_create_database_subnet_route_table
   # create_database_internet_gateway_route = true
   # create_database_nat_gateway_route = true
-  
+
   # NAT Gateways - Outbound Communication
-  enable_nat_gateway = var.vpc_enable_nat_gateway 
+  enable_nat_gateway = var.vpc_enable_nat_gateway
   single_nat_gateway = var.vpc_single_nat_gateway
 
   # VPC DNS Parameters
@@ -33,17 +33,22 @@ module "vpc" {
   enable_dns_support   = true
 
 
-  tags = local.common_tags
+  tags     = local.common_tags
   vpc_tags = local.common_tags
 
   # Additional Tags to Subnets
   public_subnet_tags = {
-    Type = "Public Subnets"
+    Type                                              = "Public Subnets"
+    "kubernetes.io/role/elb"                          = 1
+    "kubernetes.io/cluster/${local.eks_cluster_name}" = "shared"
   }
   private_subnet_tags = {
-    Type = "Private Subnets"
-  }  
+    Type                                              = "private-subnets"
+    "kubernetes.io/role/internal-elb"                 = 1
+    "kubernetes.io/cluster/${local.eks_cluster_name}" = "shared"
+  }
+
   database_subnet_tags = {
-    Type = "Private Database Subnets"
+    Type = "database-subnets"
   }
 }
